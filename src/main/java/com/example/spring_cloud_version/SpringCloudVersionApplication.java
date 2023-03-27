017bfc07994aa83df01668695348f20b7c775e76
VlNikoAd <nikolaenko_vladislav@icloud.com>
2023-03-27 19:14:19 +0300

    dockerFile and pox.xml

---
+++ src/main/java/com/example/spring_cloud_version/SpringCloudVersionApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_cloud_version;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudVersionApplication.class, args);
+	}
+
+}
